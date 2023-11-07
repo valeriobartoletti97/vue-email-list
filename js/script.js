a1edaa2b5006cb28d8977a7c55342ccc60e26d49
valeriobartoletti97 <valerio.bartoletti97@gmail.com>
2023-11-07 16:50:31 +0100

    generated emails in html

--- js/script.js
+++ js/script.js
@@ -3,10 +3,20 @@ const {createApp} = Vue;
 createApp({
     data(){
         return{
-           
+           emails: []
         }
     },
     methods:{
         
     },
+    mounted(){
+        for(let i = 0; i < 10; i++){
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((email)=>{
+            console.log(email.data.response);
+            this.emails.push(email.data.response)
+        })
+        }
+        
+        console.log(this.emails)
+    }
 }).mount('#app')
